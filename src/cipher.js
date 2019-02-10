1a513b817c61ee59f88e3fc431cc4aa5dd682ba7
JoceCasteletti <jocelyncasteletti@gmail.com>
2019-02-10 15:22:19 -0300

    se agrega fórmula de cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,35 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (offset, message)=> {
+  let resultencode ='';
+
+  for (let i=0; i< message.length; i++) {
+    let ascii_code = message.charCodeAt(i);
+
+
+    if (ascii_code >= 65 && ascii_code <=90){
+       ascii_code = ((ascii_code - 65 + offset) % 26) + 65;
+    }
+    resultencode = resultencode + String.fromCharCode(ascii_code);
+  } 
+   
+  return resultencode;
+
   },
-  decode: () => {
-    /* Acá va tu código */
+  
+  decode: (offset, message)=> {
+    let resultdecode ='';
+  
+  for (let i=0; i=message.length;i++);{
+    let ascii_decode = message.charCodeAt(i);
+  
+  
+      if (ascii_decode >= 65 && ascii_code <=90){
+         ascii_decode = ((ascii_code - 65 - offset) % 26) + 65;
+      }
+      resultdecode = resultdecode + String.fromCharCode(ascii_code);
+    } 
+     
+    return resultdecode;
+
+    }
   }
-};
