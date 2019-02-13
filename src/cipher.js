5b1c3440e2253bb6dff24afef3e988e913fba973
JoceCasteletti <jocelyncasteletti@gmail.com>
2019-02-13 01:22:06 -0300

    Se implementa versión final

--- src/cipher.js
+++ src/cipher.js
@@ -30,23 +30,27 @@ window.cipher = {
   
   decode: (offset, message) => {
     let resultdecode ='';
+    let newOffset = 0;
 
-    offset =  26 - offset;
+    
   
     for (let i=0; i < message.length; i++) {
       let asciiCode = message.charCodeAt(i);
     
     
         if (asciiCode >= 65 && asciiCode <=90) {
-          asciiCode = ((asciiCode - 65 + offset) % 26) + 65;
+          newOffset =  26 - offset;
+          asciiCode = ((asciiCode - 65 + newOffset) % 26) + 65;
         }
 
         if (asciiCode >= 97 && asciiCode <=122) {
-          asciiCode = ((asciiCode - 97 + offset) % 26) + 97;
+          newOffset =  26 - offset;
+          asciiCode = ((asciiCode - 97 + newOffset) % 26) + 97;
         }
 
-        if (asciiCode == 32) {
-          asciiCode = ((asciiCode - 32 + offset) % 1) + 32;
+        if (asciiCode >= 48 && asciiCode <=57){
+          newOffset =  10 - offset;
+          asciiCode = ((asciiCode - 48 + newOffset) % 10) + 48;
         }
 
         if (asciiCode == 32) {
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,16 +10,50 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
-  });
-
-  describe('cipher.decode', () => {
-
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.decode, 'function');
+    it('debería retornar "IJKLMNOPQRSTUVWXYZABCDEFGH" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 8',() => {
+      assert.equal(cipher.encode(8,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"),"IJKLMNOPQRSTUVWXYZABCDEFGH");      
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
+    it('debería retornar "ijklmnopqrstuvwxyzabcdegfh" para "abcdefghijklmnopqrstuvwyxz" con offset 8',() => {
+      assert.equal(cipher.encode(8,"abcdefghijklmnopqrstuvwyxz"),"ijklmnopqrstuvwxyzabcdegfh");      
+    });
+
+    it('debería retornar "8901234567" para "0123456789" con offset 8',() => {
+      assert.equal(cipher.encode(8,"0123456789"),"8901234567");      
+    });
+
+    it('debería retornar " " para " " con offset 8',() => {
+      assert.equal(cipher.encode(8," ")," ");      
+    });
+
   });
+
+
+
+describe('cipher.decode', () => {
+
+  it('debería ser un objeto', () => {
+    assert.equal(typeof cipher.decode, 'function');
+  });
+
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "IJKLMNOPQRSTUVWXYZABCDEFGH" con offset 8',()=>{
+      assert.equal(cipher.decode(8,"IJKLMNOPQRSTUVWXYZABCDEFGH"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    });
+
+
+  it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "ijklmnopqrstuvwxyzabcdefgh" con offset 8', () => {
+    assert.equal(cipher.decode(8,"ijklmnopqrstuvwxyzabcdefgh"), "abcdefghijklmnopqrstuvwxyz");
+  });
+
+  it('debería retornar "0123456789" para "8901234567" con offset 8', () => {
+    assert.equal(cipher.decode(8,"8901234567"), "0123456789");
+  });
+
+
+  it('debería retornar " " para " " con offset 8', () => {
+    assert.equal(cipher.decode(8," "), " ");
+  });
+
+});
 
 });
